0310ebcb347f4a66c988d73a1fd81135300b3383
jose.icc87 <jose.icc87@gmail.com>
2022-01-04 19:25:43 -0300

    Fix: added public routing

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoute'));  // disponibiliza: './routes/auth' -  en la ruta: '/api/auth'
 app.use('/api/events', require('./routes/eventsRoute'));  // disponibiliza: './routes/events' -  en la ruta: '/api/events'
 app.use('/login', express.static('public'));
+app.get('*', (req, res) => { res.sendFile(path.resolve(__dirname, 'public', 'index.html'));});
 
 // TODO: CRUD eventos
 
